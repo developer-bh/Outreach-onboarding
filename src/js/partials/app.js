4a6e497ccbc73dc6a4758a4f7f6ff573cd236214
bohrytsenko <hrytsenkobohdan@gmail.com>
2019-04-16 12:24:54 +0200

    Call setting screen

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,8 +1,25 @@
 $( document ).ready(function() {
 
-    // Test
+    // Phone number
 
-    console.log('test');
+    var input = document.querySelector("#phone");
+    window.intlTelInput(input);
+
+    var input = document.querySelector("#recall");
+    window.intlTelInput(input);
+
+    // Show/hide
+
+    $('.call_info__number a').on( "click", function(e) {
+        e.preventDefault();
+        var id = $(this).attr('data-related');
+        $(".call_info__number").each(function(){
+            $(this).hide();
+            if($(this).attr('id') == id) {
+                $(this).show();
+            }
+        });
+    });
 });
 
 
